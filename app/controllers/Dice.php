c26f343ce1464dfdee1da4c609c569bcc98fdea6
edvinasdau <30698620+edvinasdau@users.noreply.github.com>
2017-11-09 13:16:28 +0200

    game

---
+++ app/controllers/Dice.php
@@ -0,0 +1,49 @@
+<?php
+
+class Dice extends Controller {
+
+    public function index() {
+
+    	header("Content-type:application/json");
+
+    	$game = $this->model('Game');
+        $result = $game->getAllGames();
+
+
+    	echo json_encode($result);
+    }
+
+    public function myGames() {
+
+        header("Content-type:application/json");
+
+        $game = $this->model('Game');
+        $result = $game->getUserGames($_SESSION['username']);
+
+
+        echo json_encode($result);
+    }
+
+    public function getTopWinners() {
+
+        header("Content-type:application/json");
+
+        $game = $this->model('Game');
+        $result = $game->getTopWinners(5);
+
+
+        echo json_encode($result);
+    }
+
+    public function getTopPlayers() {
+
+        header("Content-type:application/json");
+
+        $game = $this->model('Game');
+        $result = $game->getTopPlayers(5);
+
+
+        echo json_encode($result);
+    }    
+
+}
---
+++ app/models/Game.php
@@ -0,0 +1,32 @@
+<?php
+
+class Game {
+
+    private $db;
+
+    function __construct(Database $db){
+        $this->db = $db;
+    }
+
+    public function getAllGames(): array
+    {
+         return $this->db->select("SELECT * FROM stats");
+    }
+
+    public function getUserGames(string $username): array
+    {
+       return $this->db->select("SELECT * FROM stats WHERE username = :username", ["username" => $username]);
+    }
+
+    public function getTopWinners(int $count): array
+    {
+        // TODO: Implement getTopWinners() method.
+        return $this->db->select (("SELECT result FROM stats ORDER BY result DESC LIMIT $count"),["count" => $count]);
+    }
+
+    public function getTopPlayers(int $count): array
+    {
+        // TODO: Implement getTopPlayers() method.
+        return $this->db->select ("SELECT username, count(*) as most FROM stats GROUP BY username ORDER BY most DESC LIMIT $count");
+    }
+}
